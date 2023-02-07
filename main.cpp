cee02ca50ad995f4c0c4bf03ae563d0fa91e6a71
Omer Özcan <120409490+omerstudio@users.noreply.github.com>
2023-02-07 14:27:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include "eztr.h"
-//C++ EZ-TR Ömer Studio tarafından yapılmıştır. -2023 VER: 0.1
+
+/*
+C++ EZ-TR Ömer Studio tarafından yapılmıştır. -2023 VER: 0.1
+bitir = ; (noktalı virgül)
+eger = if (if fonksiyonu)
+edel = else (else fonksiyonu)
+pac = { (açık süslü parantez)
+pkapa = } (kapalı süslü parantez)
+artix = ++ (iki tane artı while döngüsü için veya bir sayı arttırıken işe yarar)
+imai = int main() (pac ve pkapa ile kullanın)
+esit = "=" (eşittir işareti)
+eesit = "==" (iki eşittir)
+nesit = "!=" (esit değil)
+veya = || (eger (if) kullanırken "veya" anlamında)
+eve = && (eger (if) kullanırken "ve" anlamında)
+Basitleştirilmiş ve Türkçeleştirilmiş Fonksiyonlar:
+yaz (Konsola Yazı Yazar) = Örn: "yaz("Merhaba, Dünya!") bitir
+temizle (Ekranı temizler) = Örn: "temizle bitir"
+*/
 
 imai pac
 	yazi sifre bitir
