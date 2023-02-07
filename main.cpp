911eecbcdc01df9da7c63b69d4bbd822df050678
Omer Özcan <120409490+omerstudio@users.noreply.github.com>
2023-02-08 01:00:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include "eztr.h"
 
 /*
-C++ EZ-TR Ömer Studio tarafından yapılmıştır. -2023 VER: 0.1
 bitir = ; (noktalı virgül)
 eger = if (if fonksiyonu)
 edel = else (else fonksiyonu)
+egel = if else (if else fonksiyonu)
 pac = { (açık süslü parantez)
 pkapa = } (kapalı süslü parantez)
 artix = ++ (iki tane artı while döngüsü için veya bir sayı arttırıken işe yarar)
@@ -14,8 +14,13 @@ eesit = "==" (iki eşittir)
 nesit = "!=" (esit değil)
 veya = || (eger (if) kullanırken "veya" anlamında)
 eve = && (eger (if) kullanırken "ve" anlamında)
+yal = cin >> (kullanıcıdan değer alırken kullanılır)
+nxt = return 0 
+yazi = string (string yani yazı değeri belirlemek için kullanılır)
+sayi = int (sayı değerleri için kullanılır)
+dtrk = setlocale (Unicode dilini Türkçe yapar bu şekilde Türkçe karakterleri yani: ö,ç,ü ve ğ yazabilirsiniz)
 Basitleştirilmiş ve Türkçeleştirilmiş Fonksiyonlar:
-yaz (Konsola Yazı Yazar) = Örn: "yaz("Merhaba, Dünya!") bitir
+yaz (Konsola Yazı Yazar) = Örn: "yaz("Merhaba, Dünya!") bitir"
 temizle (Ekranı temizler) = Örn: "temizle bitir"
 */
 
